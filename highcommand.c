8d69cd984c43ba07af3ca096535c78be4bf55c95
Mark Old <dlom234@gmail.com>
2014-03-17 08:01:57 -0700

    untested

--- highcommand.c
+++ highcommand.c
@@ -2,28 +2,51 @@
 
 int hc_opt(char *short_name, char *long_name, char *help_text) {
     // create option
+    //   string stuffs
+    char *s = strdup(short_name);
+    char *l = strdup(long_name);
+    hc_option new_opt = {s, l, help_text, strdup(hc_meta.current_scope)};
     // add option to current scope
+    if (hc_meta.options == NULL) { // nothing
+        hc_meta.size = HC_INITIAL_OPTS_SIZE;
+        hc_meta.options = malloc(hc_meta.size * sizeof(hc_option));
+    } else if (index + 1 == size) { // full
+        hc_meta.size = hc_meta.size * 2;
+        realloc(hc_meta.options, hc_meta.size * sizeof(hc_option));
+    }
     // return success
+    return 0;
 }
 
 int hc_cmd(char *name, hc_command_callback callback) {
+    // create subcommand
     // add subcommand to current scope
-    // set scope
-    // call the callback with argv[0] being the name of the command
-    // unset scope
     // return success
+    return 0;
 }
 
 int hc_run(int argc, char *argv[]/*, hashmap *opts*/) {
     // retrieve all subcommands in current scope
     // retrieve all opts for current scope
     // check if argv has a subcommand, assuming argv[0] is either the name of the program or the name of the parent command
-    // if there's a command, run it
+    // if there's a command
+    //   set scope
+    //   call the callback with argv[0] being the name of the command
+    //   unset scope
     // else if there's options, run getopts with these opts
     // else if there's a default command, run that
     // else if default help, run that
     // else do nothing
+    // free the current scope
+    hc_free(hc_meta.current_scope);
     // return success
+    return 0;
+}
+
+int hc_free(const char *scope) {
+    // get all opts/commands in a scope
+    // reclaim that hot memory
+    return 0;
 }
 
 #ifdef HIGH_COMMAND_TEST
@@ -31,6 +54,8 @@ int hc_run(int argc, char *argv[]/*, hashmap *opts*/) {
 #include "testhelp.h"
 
 int main(int argc, char *argv[]) {
+    hc_opt("n=", "name=", "What is your name?");
+    hc_run(argc, argv);
     return 0;
 }
 
--- highcommand.h
+++ highcommand.h
@@ -1,8 +1,14 @@
 #ifndef __HIGH_COMMAND_H
 #define __HIGH_COMMAND_H
 
+#include <stdlib.h>
+#include <stddef.h>
+
+#define HC_MAX_OPTS_SIZE (1024);
+#define HC_INITIAL_OPTS_SIZE (8);
+
 typedef struct {
-    const char short_name; // not a typo
+    const char *short_name;
     const char *long_name;
     const char *help_string;
     const char *scope;
@@ -10,8 +16,16 @@ typedef struct {
 
 typedef void (*hc_command_callback)(int argc, char *argv[]);
 
+struct {
+    const hc_option *options;
+    int index;
+    int size;
+    const char *current_scope;
+} hc_meta = {NULL, 0, 0, ""};
+
 int hc_opt(char *short_name, char *long_name, char *help_text);
 int hc_cmd(char *name, hc_command_callback callback);
 int hc_run(int argc, char *argv[]/*, hashmap *opts*/);
+int hc_free(char *scope);
 
 #endif
